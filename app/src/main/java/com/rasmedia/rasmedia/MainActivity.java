d3e83981b938c9c62c3bb2f7b420c474ba923c8d
rasmedia <40476323+rasmedia@users.noreply.github.com>
2018-06-21 19:26:09 -0400

    ui update

--- app/src/main/java/com/rasmedia/rasmedia/MainActivity.java
+++ app/src/main/java/com/rasmedia/rasmedia/MainActivity.java
@@ -3,7 +3,7 @@ package com.rasmedia.rasmedia;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { //appCompatActivity is parent class MainActivity is childclass. chjild extends human.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
