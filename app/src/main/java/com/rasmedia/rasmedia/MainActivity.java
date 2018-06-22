c9fb87c777ad08a99eedb2b8804ca53e85b6f7b4
rasmedia <40476323+rasmedia@users.noreply.github.com>
2018-06-21 20:17:35 -0400

    LoginActivity Created

--- app/src/main/java/com/rasmedia/rasmedia/MainActivity.java
+++ app/src/main/java/com/rasmedia/rasmedia/MainActivity.java
@@ -2,12 +2,42 @@ package com.rasmedia.rasmedia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import static android.graphics.Color.GREEN;
+import static android.graphics.Color.RED;
 
 public class MainActivity extends AppCompatActivity { //appCompatActivity is parent class MainActivity is childclass. chjild extends human.
-
+TextView text ;
+int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        text = findViewById(R.id.textView);
+        Button button = findViewById(R.id.textButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View view) {
+
+                if (count % 2 == 0)
+                    text.setTextColor(RED);
+                else
+                    text.setTextColor(GREEN);
+                count++;
+
+
+
+            }
+        });
+
+
     }
 }
