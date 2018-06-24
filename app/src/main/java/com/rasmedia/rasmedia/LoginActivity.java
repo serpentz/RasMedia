e614b5eeaac6a6273eb8f1b7d223a49afa053054
Abyic <abyicherinet@gmail.com>
2018-06-24 00:39:31 -0400

    Merge branch 'login' of https://github.com/serpentz/RasMedia into original
    # Conflicts:
    #	app/src/main/res/layout/activity_login.xml

--- app/src/main/java/com/rasmedia/rasmedia/LoginActivity.java
+++ app/src/main/java/com/rasmedia/rasmedia/LoginActivity.java
@@ -1,9 +1,15 @@
 package com.rasmedia.rasmedia;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -18,12 +24,56 @@ public class LoginActivity extends AppCompatActivity {
 
 
         layout = findViewById(R.id.relative_layout_login);
+
         // onCreate
         animationDrawable =(AnimationDrawable)layout.getBackground();
         animationDrawable.setEnterFadeDuration(5000);
         animationDrawable.setExitFadeDuration(5000);
-// onResume
+
+        // onResume
         animationDrawable.start();
+
+        Button loginButton = findViewById(R.id.loginButton);
+        final EditText loginEmail = findViewById(R.id.loginEmail);
+        final EditText loginPassword = findViewById(R.id.loginPassword);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String emailString = loginEmail.getText().toString();
+                String passwordEmail = loginPassword.getText().toString();
+
+                validateLogin(emailString,passwordEmail);
+//                Intent toMainActivity = new Intent(LoginActivity.this,MainActivity.class);
+//                startActivity(toMainActivity);
+
+                Log.e("Login Activity"," login button pressed");
+
+
+            }
+        });
+
+
+
+
+    }
+
+    private void validateLogin(String emailString, String passwordEmail) {
+        //@TODO connect to server
+
+    }
+
+    public void OnSuccoes(){
+
+        Intent toMainActivity = new Intent(LoginActivity.this,MainActivity.class);
+        startActivity(toMainActivity);
+
+
+    }
+
+    public void OnFailure( String Message){
+
 
     }
 }
