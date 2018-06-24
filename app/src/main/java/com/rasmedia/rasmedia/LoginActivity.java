b203d791da9fafe7233d6fce0a5a1e4fec287802
Abyic <abyicherinet@gmail.com>
2018-06-23 23:30:27 -0400

    Merge branch 'login' of https://github.com/serpentz/RasMedia into original
    # Conflicts:
    #	app/src/main/res/layout/activity_login.xml

--- app/src/main/java/com/rasmedia/rasmedia/LoginActivity.java
+++ app/src/main/java/com/rasmedia/rasmedia/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.rasmedia.rasmedia;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    private RelativeLayout layout;
+    private AnimationDrawable animationDrawable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        layout = findViewById(R.id.relative_layout_login);
+        // onCreate
+        animationDrawable =(AnimationDrawable)layout.getBackground();
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(5000);
+// onResume
+        animationDrawable.start();
+
     }
 }
